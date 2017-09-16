31d3b18d2007ac13a64de166bb7673b0903b4164
Anthematics <jasonharder2@gmail.com>
2017-09-16 16:18:55 -0400

    can now post articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+
+  def show
+  @article = Article.find(params[:id])
+  end
+
   def new
   end
 
@@ -9,8 +14,9 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
+
 private
   def article_params
     params.require(:article).permit(:title, :text)
-  end 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/articles/:id'=> 'articles#show'
-
-
-
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
