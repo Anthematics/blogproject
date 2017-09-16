cbac88f0bc319ae72c6740aeffabc33079758f6c
Anthematics <jasonharder2@gmail.com>
2017-09-16 18:08:35 -0400

    Blog V 1.0 CRUD fully works

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   get '/articles/:id'=> 'articles#show'
 
   get '/articles' => 'articles#index'
+
+  get '/articles/:id' => 'articles#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
